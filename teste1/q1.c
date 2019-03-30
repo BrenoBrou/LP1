9779230bf7325789d50629af90801c42a38fb0d0
BrenoBrou <49043302+BrenoBrou@users.noreply.github.com>
2019-03-29 21:40:20 -0300

    Create q1.c

---
+++ teste1/q1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void){
+    int num, soma=0;
+    printf("Escolha um número: ");
+    scanf("%d", &num);
+    while (0){
+    if (num<=0);
+    break;
+    soma=(soma+num);
+    num=(num-1);
+}
+    printf("Somou:"), ("%d\n",soma);
+return 0;
+}
